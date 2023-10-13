ce69a4b05dfd98e450e0720a69fb68851055637c
이아란(Aran Lee) <81959334+araaaaan@users.noreply.github.com>
2023-10-13 21:53:07 +0200

    Update shell script

--- monitoring.sh
+++ monitoring.sh
@@ -1,32 +1,80 @@
+
 #!/bin/bash
 
+arch = $(uname -a)
+phycpu = $(nproc)
+vcpu = 
+totalmem = $(free -m | grep 'Mem' | awk  '{print $2}')
+usedmem = $(free -m | grep 'Mem' | awk  '{print $3}')
+resultmem = 
+
+totaldisk = 
+useddisk =
+resultdisk =
+cpuload = 
+lastboot = $(who -b | awk '{print $3,$4}')
+lvmuse = $(lvscan | grep -q 'ACTIVE' && echo "yes" || echo "no")
+ctpc = $(ss -tunlp| grep 'tcp' | wc -l)
+ulog = $(who | wc -l)
+network1 = $(hostname -I)
+network2 = $(ip addr | grep 'link/ether'| awk '{print $2}')
+sucom =
+	
 echo "#Architecture: $(uname -a)"
+#The nproc command basically displays in output the number of available processing units. 
 echo "#CPU physical : 
-
-$(lscpu -b -p=Core,Socket | grep -v '^#' | sort -u | wc -l)"
-lscpu : information of CPU architecture 
--b : print online CPUs only, 
--p / -e : output [CPU, CORE...]
-
-$(cat /proc/cpuinfo | grep 'cpu cores' | wc -l)"
-
-echo "#vCPU : $(nproc)"
-
+ $(nproc)"
+#$(lscpu -b -p=Core,Socket | grep -v '^#' | sort -u | wc -l)"
+#lscpu : information of CPU architecture 
+#-b : print online CPUs only, 
+#-p / -e : output [CPU, CORE...]
+echo "#vCPU : $(cat /proc/cpuinfo | grep 'cpu cores' | wc -l)"???
+#cat /proc/cpuinfo | grep  processor
 echo "#Memory Usage: $(free)"
 free -h : human friendly output of RAM...?
 vmstat
 top : check RAM usage
 cat /proc/meminfo : RAM info
+#free -m | grep 'Mem' | awk  ' {print $2}' ####total
+#free -m | awk  '$1=="Mem:" {print $2}'
+#free -m | grep 'Mem' | awk  ' {print $3}' ### used 
+#free -m | awk  '$1=="Mem:" {print $3}'
 echo "#Disk Usage: $()"
 echo "#CPU load: $()"
 echo "#Last boot: $()"
-echo "#LVM use: $()"
-vgdisplay
-pvdisplay
-lvscan : check it is active or inactive
-lvscan | grep -q 'ACTIVE' && echo "#LVM use : yes" || echo "#LVM use : no"
+echo "#LVM use: $(lvscan | grep -q 'ACTIVE' && echo "#LVM use : yes" || echo "#LVM use : no")"
+#vgdisplay
+#pvdisplay
+#lvscan : check it is active or inactive
+#lvscan | grep -q 'ACTIVE' && echo "#LVM use : yes" || echo "#LVM use : no"
 echo "#Connections TPC : $()"
+#ss -tunlp| grep 'tcp' | wc -l
 echo "#User log: $()"
-echo "#Network: $()"
-echo "#Sudo: $() cmd"
+#who -u | wc -l  or who | wc -l
+#echo "#User log: $(who | wc -l)"
+#who | grep tty | wc -l
+echo "#Network: IP $(hostname -I)($(ip addr | grep 'link/ether'| awk '{print $2}'))" 
+#ip addr | grep 'link/ether'| awk '{print $2}'
+#echo "#Network: IP $(hostname -I)($(ip addr | grep 'link/ether'| awk '{print $2}'))" 
+#echo "($(ip addr | grep 'link/ether'| awk '{print $2}'))" (MAC address)
 
+echo "#Sudo: $() cmd"
+# journalctl | grep 'COMMAND=' | wc -l
+#echo "#Sudo: $(journalctl | grep 'COMMAND=' | wc -l) cmd"
+#journalctl : ournalctl will display your logs in a format similar to the traditional syslog format. Each line starts with the date (in the server’s local time), followed by the server’s hostname, the process name, and the message for the log.
+#sudo journalctl | wc -l
+
+
+wall ' 
+	#Architecture: $arch
+	#CPU physical : $phycpu
+	#vCPU : $vcpu
+	#Memory Usage: $usedmem/$totalmem MB ($resultmem%)
+	#Disk Usage: $useddisk/$totaldiskGb ($resultdisk%)
+	#CPU load: $cpuload%
+	#Last boot: $lastboot
+	#LVM use: $lvmuse
+	#Connections TPC : $ctpc ESTABLISHED
+	#User log: $ulog
+	#Network: IP $network1 $network2
+	#Sudo: $sucom cmd'
